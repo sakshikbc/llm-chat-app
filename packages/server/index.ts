6c4bb3b526bd8c0e37105565180ce556c83d9447
Sakshi <sakshikbc@gmail.com>
2025-12-30 14:45:37 +0530

    Connect client & server

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -7,8 +7,10 @@ dotenv.config()
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send("Hello world")
+app.get('/api/hello', (req: Request, res: Response) => {
+    res.json({
+        "message" : "Hello World"
+    })
 });
 
 app.listen(port, () => {
