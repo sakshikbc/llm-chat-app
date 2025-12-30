7280d03c0c931689a3a5b0c05c0eaeb647d80177
Sakshi <sakshikbc@gmail.com>
2025-12-30 15:29:47 +0530

    Setup husky

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
 
 import './index.css';
-import { Button } from './components/ui/button';
+import { Button } from './components/ui/button'
 
 function App() {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState('')
 
   useEffect(() => {
     fetch('/api/hello')
@@ -13,10 +13,10 @@ function App() {
   }, []);
 
   return (
-    <div className="p-4">
-      <p className="font-bold text-3xl">{message}</p>
-      <Button>Click Me</Button>
-    </div>
+  <div className="p-4">
+    <p className="font-bold text-3xl">{message}</p>
+    <Button>Click Me</Button>
+  </div>
   );
 }
 
