04e57c9fd956b470e4a148e8cc066f77b008036b
Sakshi <sakshikbc@gmail.com>
2026-01-04 12:24:07 +0530

    Chat bot

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,21 +1,12 @@
-import { useEffect, useState } from 'react';
 
 import './index.css';
-import { Button } from './components/ui/button'
+import ChatBot from './components/chat/ChatBot';
 
 function App() {
-  const [message, setMessage] = useState('')
-
-  useEffect(() => {
-    fetch('/api/hello')
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
 
   return (
-    <div className="p-4">
-      <p className="font-bold text-3xl">{message}</p>
-      <Button>Click Me</Button>
+    <div className="p-4 h-screen w-[60%] mx-auto justify-center">
+      <ChatBot />
     </div>
   );
 }
